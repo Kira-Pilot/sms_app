6a1c9e9f4bc6456b944d4a92c51b7b679bfc03a1
Kira Pilot <kira.pilot23@gmail.com>
2016-08-03 14:18:41 -0400

    adding to GitHub
    changing text message

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      
+      
+      # Instantiate a Twilio client
+      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+      
+      # Create and send an SMS message
+      message=client.account.messages.create({
+        from: TWILIO_CONFIG['from'],
+        to: @user.phone,
+        body: "Hi! This is Kira."
+      })
+
+      puts message.to
+
+      render text: "Thank you! You will receive an SMS shortly with verification instructions."
+
+    else
+      render :new
+    end
+  end
+
+end
